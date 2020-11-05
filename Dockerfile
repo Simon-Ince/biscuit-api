bb8d40b1294b4022e6a805c203a6b62f61c89d19
Simon-Ince <simon.w.ince@gmail.com>
2020-11-05 21:31:59 +0000

    init commit

---
+++ Dockerfile
@@ -0,0 +1,13 @@
+# Use the official Python image.
+# https://hub.docker.com/_/python
+FROM python:3.7
+
+# Copy local code to the container image.
+ENV APP_HOME /app
+WORKDIR $APP_HOME
+COPY ./app .
+
+# Install production dependencies.
+RUN pip install --no-cache-dir -r requirements.txt
+
+CMD exec gunicorn --bind :$PORT --workers 1 --threads 8 app:app
---
+++ app/app.py
@@ -0,0 +1,111 @@
+import os  # for grbbing os variables
+import base64  # for decoding os variables
+from flask import Flask, request, render_template, jsonify  # for web
+from datetime import datetime  # for working with time
+import pytz  # for working with timezones
+
+# for Google Sheets
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+
+
+"""
+Flask Setup
+"""
+app = Flask(__name__)
+app.debug = os.getenv("DEBUG", True)
+
+"""
+Google Sheets Setup
+"""
+# Setup creds
+gcreds = os.getenv("GCREDS", False)
+
+if not os.path.exists("/creds"):
+    os.makedirs("/creds")
+
+with open("/creds/gcreds.json", "wb") as fh:
+    fh.write(base64.b64decode(gcreds))
+
+# use creds to create a client to interact with the Google Drive API
+scope = ["https://spreadsheets.google.com/feeds"]
+creds = ServiceAccountCredentials.from_json_keyfile_name("/creds/gcreds.json", scope)
+client = gspread.authorize(creds)
+
+"""
+Date & Time
+"""
+# set timezone to London
+tz = pytz.timezone("Europe/London")
+
+# determine time of day by hour
+def time_of_day(hour):
+    return (
+        "Morning"
+        if 10 <= hour <= 12
+        else "Afternoon"
+        if 13 <= hour <= 18
+        else "Evening"
+        if 19 <= hour <= 22
+        else "Night"
+        if 19 <= hour <= 22
+        else None
+    )
+
+
+"""
+Google Sheets
+"""
+
+# class for Google Sheet as an object
+class Gs:
+    def __init__(self):
+        self.sheet = client.open_by_key("12oECP06QG6bgJ8LnpNawvJ0kRCIUm4Ej3TSewUQBKoM")
+        self.values = self.read_all()
+
+    def read_all(self):
+        list_of_hashes = self.sheet.sheet1.get_all_records()
+        return list_of_hashes
+
+    def acceptable_now(self, date_time=None):
+
+        # if the date_time has not been given...
+        if date_time is None:
+            # grab the current hour
+            date_time = datetime.now(tz)
+
+        # determine the time of day by Sophia's rules
+        _time_of_day = time_of_day(date_time.hour)
+
+        # prep our list of acceptable biscuits
+        acceptable = {"time": _time_of_day, "date_time": date_time, "list": []}
+
+        # check if each of our biscuits is acceptable or not
+        for biscuit in self.values:
+            # check it the biscuit is acceptable at this time of day
+            if biscuit[_time_of_day] == "TRUE":
+                # check it the biscuit is acceptable this month
+                if (
+                    date_time.strftime("%B") in biscuit["Acceptable Months"].split(",")
+                    or biscuit["Acceptable Months"] == ""
+                ):
+                    acceptable["list"].append(biscuit)
+
+        return acceptable
+
+
+"""
+Routes
+"""
+
+# index page
+@app.route("/", methods=["GET"])
+def index():
+    # create Google Sheet object
+    sheet = Gs()
+
+    return render_template("index.html", data=sheet.acceptable_now())
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 80)))
